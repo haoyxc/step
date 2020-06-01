ef78d4439b775ef531cf25716734c94d422282ae
Cindy Hao <cindyup@gmail.com>
2020-06-01 14:03:00 -0500

    add functionality to display each message in an unordered list

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -89,6 +89,13 @@ function loadSkills () {
  */
  async function fetchFromData() {
   const response = await fetch('/data');
-  const quote = await response.text();
-  document.getElementById('secret-message').innerText = quote; 
+
+  // This gives a list of messages
+  const msgs = await response.json();
+
+  // Adds all of the messages to an unordered list
+  msgs.forEach(m => {
+    const node = `<li class="msg">${m}</li>`; 
+    $("#secret-message").append(node); 
+  }); 
  }
