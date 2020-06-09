d5d05cf7943299e03c55e4f9a0b49e050eff89e3
Cindy Hao <cindyup@gmail.com>
2020-06-09 17:32:44 +0000

    add function for loading the map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -199,3 +199,10 @@ function correctDisplay() {
     fetchFromData();
   }
 }
+
+function createMap() {
+  console.log("HELLP")
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
